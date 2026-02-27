651580311fcec253aafdaf79fadd246e9b821693
agboyano <agboyano@protonmail.com>
2026-02-27 14:16:28 +0100

    misc

---
+++ fuchitools/misc.py
@@ -0,0 +1,15 @@
+import logging
+
+def stream_logger(module_name, level="ERROR"):
+
+
+    logger = logging.getLogger(module_name)
+    logger.setLevel(getattr(logging, level)) 
+
+    handler = logging.StreamHandler()
+    handler.setLevel(getattr(logging, level))
+
+    logger.addHandler(handler)
+    return logger 
+
+
